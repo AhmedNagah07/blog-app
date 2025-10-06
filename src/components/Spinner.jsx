3d0cdd5b829649d0467b4d479295f64e18dfc9f8
AhmedNagah07 <ahmednagah073@gmail.com>
2025-10-06 09:48:06 +0300

    Add new updates to spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -5,11 +5,12 @@ export default function Spinner() {
     <div className="flex flex-col items-center justify-center p-6 space-y-3">
       <div
         className={`
-          $w-100 h-100 border-6
-          border-t-orange-500 border-r-orange-500 border-b-gray-700 border-l-gray-700 
+          w-100 h-100 border-8 
+          border-transparent 
+          border-x-blue-600 
           rounded-full 
           animate-spin 
-          shadow-lg
+          shadow-md
         `}
       ></div>
     </div>
